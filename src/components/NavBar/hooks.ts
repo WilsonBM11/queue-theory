b4e2c87f1b7257a0d744b2320e9fd64ef0219396
Wilson <wilsonbm11@gmail.com>
2024-06-30 14:58:49 -0600

    Se arreglan rutas

--- src/components/NavBar/hooks.ts
+++ src/components/NavBar/hooks.ts
@@ -4,11 +4,11 @@ export const useDependencies = () => {
 	const navigate = useNavigate();
 
 	const items = [
-		{ key: 0, label: 'Home', url: '/' },
-		{ key: 1, label: 'M/M/1', url: '/MM1' },
-		{ key: 2, label: 'M/M/m', url: '/MMm' },
-		{ key: 3, label: 'M/D/1', url: '/MD1' },
-		{ key: 4, label: 'M/M/1 PF', url: '/MM1PF' },
+		{ key: 0, label: 'Home', url: '/queue-theory/' },
+		{ key: 1, label: 'M/M/1', url: '/queue-theory/MM1' },
+		{ key: 2, label: 'M/M/m', url: '/queue-theory/MMm' },
+		{ key: 3, label: 'M/D/1', url: '/queue-theory/MD1' },
+		{ key: 4, label: 'M/M/1 PF', url: '/queue-theory/MM1PF' },
 	];
 
 	const handleClick = (e: { key: string }) => {
