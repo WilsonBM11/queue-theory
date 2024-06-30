80651de0e28a878def0ae33bb650d66c3b1031ab
Wilson <wilsonbm11@gmail.com>
2024-06-30 14:54:33 -0600

    Comentarios

--- src/pages/MM1/hooks.ts
+++ src/pages/MM1/hooks.ts
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
 const useDependencies = () => {
-	const [lambda, setLambda] = useState<number | null>(null);
-	const [mu, setMu] = useState<number | null>(null);
+	const [lambda, setLambda] = useState<number | null>(null); // Promedio de Llegadas por Periodo (horas, min)
+	const [mu, setMu] = useState<number | null>(null); // Promedio de Clientes o Unidades Atendidas por Periodo
 	const [k, setK] = useState<number | null>(null);
 	const [results, setResults] = useState<{
 		L?: number;
@@ -16,12 +16,12 @@ const useDependencies = () => {
 
 	const calculateResults = () => {
 		if (lambda !== null && mu !== null && k !== null) {
-			const L = lambda / (mu - lambda);
-			const W = 1 / (mu - lambda);
-			const Lq = lambda ** 2 / (mu * (mu - lambda));
-			const Wq = lambda / (mu * (mu - lambda));
-			const p = lambda / mu;
-			const Po = 1 - lambda / mu;
+			const L = lambda / (mu - lambda); // Promedio de Clientes o Unidades en el Sistema
+			const W = 1 / (mu - lambda); // Tiempo Promedio de Clientes o Unidades en el Sistema
+			const Lq = lambda ** 2 / (mu * (mu - lambda)); // Promedio de Clientes o Unidades en la Cola
+			const Wq = lambda / (mu * (mu - lambda)); // Tiempo Promedio de Clientes o Unidades en la Cola
+			const p = lambda / mu; // Probabilidad de Uso del Sistema
+			const Po = 1 - lambda / mu; // Tiempo ocioso, Probabilidad de Nadie en el Sistema
 			const PnK = (lambda / mu) ** (k + 1);
 			setResults({ L, W, Lq, Wq, p, Po, PnK });
 			console.log(results);
